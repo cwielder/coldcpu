3c1eb923d943a2cd3b3aa9ad4f383699c61283b6
cwielder <160283747+cwielder@users.noreply.github.com>
2024-09-08 23:53:07 -0400

    Source code for tests

---
+++ workdir/fibonacci.asm
@@ -0,0 +1,24 @@
+SETI r3, 9
+BL 2
+SYSCALL HALT
+STW r0, r0, -24
+SUBI r0, r0, 24
+STW r31, r0, 12
+SET r31, r3
+CMPI r3, 1
+BLE 13
+MFLR r1
+STW r1, r0, 20
+STW r30, r0, 8
+SUBI r3, r3, 1
+BL -10
+SET r30, r3
+SUBI r3, r31, 2
+BL -13
+ADD r3, r30, r3
+LDW r30, r0, 8
+LDW r1, r0, 20
+MTLR r1
+LDW r31, r0, 12
+ADDI r0, r0, 24
+BLR
---
+++ workdir/link_register.asm
@@ -0,0 +1,6 @@
+SETI r0, '%'
+BL 3
+SYSCALL PRINT, r0
+SYSCALL HALT
+SETI r0, '~'
+BLR
